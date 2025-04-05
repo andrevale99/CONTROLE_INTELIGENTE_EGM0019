6fc36414e725b14ba317e38bab1b420991d1c255
andrevale99 <andrefvale@gmail.com>
2025-04-05 07:45:36 -0300

    Leitura do ADC sem interrupção.

--- Projeto/main.c
+++ Projeto/main.c
@@ -1,21 +1,21 @@
 /**
- * @author Andre Menezes de Freitas Vale
- *
- * @brief Codigo que utiliza da interrupcao externas PCINTXX
- * para criar uma logica de semaforo simples
- *
- * @note O PWM é gerado pelo pino PB1 (OC1A) sem o auxilio de interrupcao,
- * pois a configuracao do TIMER 1 permite isso, ou seja, a mudanca ocorrera
- * de forma assincrona.
- *
- * Utiliza um potenciometro para mudar a frequencia, lendo a
- * entrada analogica, e modificando o valor do OCR1A.
- *
- * Uma forma de melhorar ainda mais o codigo, seria de utilizar a
- * interrupcao de vez a programacao procedural, mas como o pino PB1
- * esta no modo "assincrono", nao tem necessidade disso.
- *
- */
+   @author Andre Menezes de Freitas Vale
+
+   @brief Codigo que utiliza da interrupcao externas PCINTXX
+   para criar uma logica de semaforo simples
+
+   @note O PWM é gerado pelo pino PB1 (OC1A) sem o auxilio de interrupcao,
+   pois a configuracao do TIMER 1 permite isso, ou seja, a mudanca ocorrera
+   de forma assincrona.
+
+   Utiliza um potenciometro para mudar a frequencia, lendo a
+   entrada analogica, e modificando o valor do OCR1A.
+
+   Uma forma de melhorar ainda mais o codigo, seria de utilizar a
+   interrupcao de vez a programacao procedural, mas como o pino PB1
+   esta no modo "assincrono", nao tem necessidade disso.
+
+*/
 
 #ifndef F_CPU
 #define F_CPU 16000000UL
@@ -31,7 +31,7 @@
 //  VARIAVEIS
 //===================================================
 
-uint16_t limit = 0;
+uint16_t adcResult = 0;
 
 //===================================================
 //  PROTOTIPOS
@@ -39,19 +39,27 @@ uint16_t limit = 0;
 
 void setup_pwm_phase_correct(void);
 
+
+void adc_setup(void);
+uint16_t adc_read(uint8_t pino);
+
 //===================================================
 //  MAIN
 //===================================================
 int main()
 {
-    setup_pwm_phase_correct();
+  setup_pwm_phase_correct();
+  adc_setup();
 
-    for (;;)
-    {
+  Serial.begin(115200);
 
-    }
+  for (;;)
+  {
+    adcResult = adc_read(0x00);
+    Serial.println((adcResult));
+  }
 
-    return 0;
+  return 0;
 }
 //===================================================
 //  FUNCOES
@@ -59,16 +67,58 @@ int main()
 
 void setup_pwm_phase_correct(void)
 {
-    // Pino OC1A (PORTB1) como OUTPUT para PWM
-    DDRB |= (1 << DDB1);
+  // Pino OC1A (PORTB1) como OUTPUT para PWM
+  DDRB |= (1 << DDB1);
 
-    // OC1A clear on compare match
-    TCCR1A |= (1 << COM1A1) | (1 << WGM11);
+  // OC1A clear on compare match
+  TCCR1A |= (1 << COM1A1) | (1 << WGM11);
 
-    // Prescale de 1024 no modo Fast PWM com o TOP em ICR1
-    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS12) | (1 << CS10);
+  // Prescale de 1024 no modo Fast PWM com o TOP em ICR1
+  TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS12) | (1 << CS10);
 
-    OCR1A = (uint16_t)150;
+  OCR1A = (uint16_t)15000 * 1;
 
-    ICR1 = 15000;
+  ICR1 = 15000;
+}
+
+/**
+   @brief Inicializacao do ADC
+
+   @note Ativa o ADC com o prescale de 16e6/128 e
+   referencia no AVCC/AREF
+*/
+void adc_setup()
+{
+  ADCSRA |= (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+  ADMUX |= (1 << REFS0);
+
+}
+
+/**
+   @brief Funcao para pegar o valor do adc
+   @param pino Pino que deseja ler
+   @return Valor da conversao ADC em um valor de 16 bits
+
+   @note Retorna um valor de 16 bits pela facilidade,
+   ja que a resolucao do arduino e de 10 bits
+*/
+uint16_t adc_read(uint8_t pino)
+{
+  static uint8_t adc_LSB;
+  static uint8_t adc_MSB;
+
+  ADMUX |= pino;
+
+  ADCSRA |= (1 << ADSC);
+
+  while (!(ADCSRA &= ~(1 << ADIF)));
+
+  ADCSRA |= (1 << ADIF);
+
+  adc_LSB = ADCL;
+  adc_MSB = ADCH;
+
+  ADCSRA |= (1 << ADSC);
+
+  return (adc_MSB << 8) | adc_LSB;
 }
