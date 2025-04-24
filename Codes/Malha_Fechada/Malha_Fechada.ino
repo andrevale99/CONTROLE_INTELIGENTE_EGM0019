3c024ae0ca3b195a516f46311680d54b46e34e34
andrevale99 <andrefvale@gmail.com>
2025-04-24 12:01:00 -0300

    Update

--- Codes/Malha_Fechada/Malha_Fechada.ino
+++ Codes/Malha_Fechada/Malha_Fechada.ino
@@ -1,5 +1,7 @@
 #define POT_PIN A0
 
+#define BUTAO_PIN_PWM 9
+
 #define PONTE_H_1 7
 #define PONTE_H_2 6
 #define PONTE_H_ENABLE 5
@@ -9,11 +11,17 @@
 
 #define PULSOS_POR_VOLTA (float)341.2
 
-#define PERIODO 100 // ms
+#define PERIODO 50  // ms
 
 //=====================================================
 //  VARIAVEIS GLOBAIS
 //=====================================================
+
+int PWM = 0;
+
+static float controle = 0;
+
+float teste_controle = 0;
 
 volatile long PulsosCanalA = 0;
 long QtdPulsosCanalA = 0;
@@ -23,6 +31,12 @@ volatile long long ultimoTempo = 0;
 float theta = 0;
 float omega = 0;
 
+static float acaoP = 0.;
+static float acaoI = 0.;
+static float erro = 0.;
+static float Kp = 0.5;
+static float Ki = 0.02;
+
 unsigned long long TempoInicialCalculo = 0;
 unsigned long TempoTotalCalculo = 0;
 
@@ -31,13 +45,13 @@ unsigned long TempoTotalCalculo = 0;
 //=====================================================
 
 void atualizaEncoder(void);
+void aciona_motor(float controle, int PWM);
 
 //=====================================================
 //  SETUP E LOOP
 //=====================================================
 
-void setup()
-{
+void setup() {
   Serial.begin(115200);
 
   pinMode(PONTE_H_1, OUTPUT);
@@ -47,24 +61,18 @@ void setup()
   pinMode(CANAL_ENCODER_A, INPUT_PULLUP);
   pinMode(CANAL_ENCODER_B, INPUT_PULLUP);
 
+  pinMode(BUTAO_PIN_PWM, INPUT_PULLUP);
+
   attachInterrupt(digitalPinToInterrupt(CANAL_ENCODER_A),
                   atualizaEncoder, FALLING);
 }
 
-void loop()
-{
-  static float erro = 0.;
-  static float Kp = 0.2;
-  static float Ki = 0.005;
-  static float acaoP = 0.;
-  static float acaoI = 0.;
-  static float controle = acaoP + acaoI;
+void loop() {
 
-  int sp = map(analogRead(POT_PIN), 0, 1023, -160, 160);
+  int sp = map(analogRead(POT_PIN), 0, 1023, -180, 180);
   unsigned long tempo = millis();
 
-  if ((tempo - ultimoTempo) >= PERIODO || ultimoTempo == 0)
-  {
+  if ((tempo - ultimoTempo) >= PERIODO || ultimoTempo == 0) {
     ultimoTempo = tempo;
     detachInterrupt(digitalPinToInterrupt(CANAL_ENCODER_A));
 
@@ -78,7 +86,7 @@ void loop()
 
     erro = sp - omega;
     acaoP = Kp * erro;
-    acaoI += Ki * erro * PERIODO;
+    acaoI += Ki * erro * PERIODO / 1000;
 
     PulsosPorPeriodo = 0;
 
@@ -88,62 +96,62 @@ void loop()
                     atualizaEncoder, FALLING);
   }
 
-  controle = acaoP + acaoI;
-  int PWM = min(abs(controle), 255);
+    controle = acaoP + acaoI;
 
-  if (vel >= 0)
-  {
-    digitalWrite(PONTE_H_1, LOW);
-    digitalWrite(PONTE_H_2, HIGH);
+  if (digitalRead(BUTAO_PIN_PWM) == 0) {
+    PWM = min(controle, 255);
+    aciona_motor(controle, PWM);
   }
-  else
-  {
-    digitalWrite(PONTE_H_1, HIGH);
-    digitalWrite(PONTE_H_2, LOW);
-  }
-  analogWrite(PONTE_H_ENABLE, PWM);
+
 
   Serial.print((float)ultimoTempo / 1000, 3);
   Serial.print("\t");
-  Serial.print(TempoTotalCalculo);
+  Serial.print(sp);
   Serial.print("\t");
-  Serial.print(valPot);
-  Serial.print("\t");
-  Serial.print(vel);
-  Serial.print("\t");
+  // Serial.print(TempoTotalCalculo);
+  // Serial.print("\t");
   Serial.print(PWM);
   Serial.print("\t");
-  Serial.print(QtdPulsosCanalA);
+  // Serial.print(QtdPulsosCanalA);
+  // Serial.print("\t");
+  // Serial.print(QtdPulsoPorPeriodo);
+  // Serial.print("\t");
+  // Serial.print(theta);
+  // Serial.print("\t");
+  Serial.print(omega);
   Serial.print("\t");
-  Serial.print(QtdPulsoPorPeriodo);
+  Serial.print(erro);
   Serial.print("\t");
-  Serial.print(theta);
+  Serial.print(acaoP);
   Serial.print("\t");
-  Serial.println(omega);
+  Serial.print(acaoI);
   Serial.print("\t");
-  Serial.println(erro);
-  Serial.print("\t");
-  Serial.println(acaoP);
-  Serial.print("\t");
-  Serial.println(acaoI);
-  Serial.print("\t");
-  Serial.println(controle);
+  Serial.print(controle);
+
+  Serial.print("\n");
 }
 
 //=====================================================
 //  FUNCS
 //=====================================================
 
-void atualizaEncoder(void)
-{
-  if (digitalRead(CANAL_ENCODER_A) == digitalRead(CANAL_ENCODER_B))
-  {
+void atualizaEncoder(void) {
+  if (digitalRead(CANAL_ENCODER_A) == digitalRead(CANAL_ENCODER_B)) {
     PulsosCanalA++;
     PulsosPorPeriodo++;
-  }
-  else
-  {
+  } else {
     PulsosCanalA--;
     PulsosPorPeriodo--;
   }
+}
+
+void aciona_motor(float controle, int PWM) {
+  if (controle >= 0) {
+    digitalWrite(PONTE_H_1, LOW);
+    digitalWrite(PONTE_H_2, HIGH);
+  } else {
+    digitalWrite(PONTE_H_1, HIGH);
+    digitalWrite(PONTE_H_2, LOW);
+  }
+  analogWrite(PONTE_H_ENABLE, PWM);
 }
