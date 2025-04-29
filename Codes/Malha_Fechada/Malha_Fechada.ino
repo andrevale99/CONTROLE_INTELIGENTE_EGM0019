933ff88c70cfa365958aaed35b4add8dd6da6b10
andrevale99 <andrefvale@gmail.com>
2025-04-29 11:53:24 -0300

    Correção

--- Codes/Malha_Fechada/Malha_Fechada.ino
+++ Codes/Malha_Fechada/Malha_Fechada.ino
@@ -2,7 +2,7 @@
 
 #define BUTAO_PIN_PWM 9
 
-#define PONTE_H_1 5 // 7
+#define PONTE_H_1 7 // 7
 #define PONTE_H_2 6
 #define PONTE_H_ENABLE 5 // 5
 
@@ -13,9 +13,9 @@
 
 #define PI 3.1415
 
-#define PERIODO 50 // ms
+#define PERIODO 100 // ms
 
-#define PWM_MAX_LIMIT (float)160.0
+#define PWM_MAX_LIMIT 160
 
 //=====================================================
 //  VARIAVEIS GLOBAIS
@@ -38,8 +38,8 @@ float omega = 0;
 static float acaoP = 0.;
 static float acaoI = 0.;
 static float erro = 0.;
-static float Kp = 3.;
-static float Ki = 10.;
+static float Kp = 1;
+static float Ki = 0.01;
 
 unsigned long long TempoInicialCalculo = 0;
 unsigned long TempoTotalCalculo = 0;
@@ -78,7 +78,7 @@ void setup()
 void loop()
 {
 
-  sp = map(analogRead(POT_PIN), 0, 1023, -PWM_MAX_LIMIT, PWM_MAX_LIMIT);
+  sp = map(analogRead(POT_PIN), 0, 1023, -160, 160);
   unsigned long tempo = millis();
 
   if ((tempo - ultimoTempo) >= PERIODO || ultimoTempo == 0)
@@ -91,26 +91,25 @@ void loop()
     QtdPulsosCanalA = PulsosCanalA;
     QtdPulsoPorPeriodo = PulsosPorPeriodo;
 
-    // theta = PulsosCanalA / PULSOS_POR_VOLTA * 360.f;
-    theta = PulsosPorPeriodo * 2 * PI / PULSOS_POR_VOLTA;
-    // omega = (PulsosPorPeriodo / PULSOS_POR_VOLTA) * (60000 / PERIODO);
-    omega = theta / (PERIODO / 1000.0f) * (60 / (2 * PI));
+    theta = PulsosCanalA / PULSOS_POR_VOLTA * 360.f;
+    //theta = PulsosPorPeriodo * 2 * PI / PULSOS_POR_VOLTA;
+    omega = (PulsosPorPeriodo / PULSOS_POR_VOLTA) * (60000 / PERIODO);
+    //omega = theta / (PERIODO / 1000.0f) * (60 / (2 * PI));
 
     erro = sp - omega;
     acaoP = Kp * erro;
-    acaoI += Ki * erro * (float)PERIODO / 1000.;
+    acaoI += Ki * erro * (float)PERIODO;
 
     PulsosPorPeriodo = 0;
 
     TempoTotalCalculo = float(micros() - TempoInicialCalculo);
 
-    show_data();
-
+    
     attachInterrupt(digitalPinToInterrupt(CANAL_ENCODER_A),
                     atualizaEncoder, FALLING);
-  }
+    show_data();
 
-  controle = acaoP + acaoI;
+  }
 
   // anti-windup (para nao ficar
   // incrementando a integração infinitamente)
@@ -119,15 +118,18 @@ void loop()
   if (acaoI < -PWM_MAX_LIMIT)
     acaoI = -PWM_MAX_LIMIT;
 
+    
+  controle = acaoP + acaoI;
+
   // anti-windup (para nao ficar
   // incrementando a integração infinitamente)
-  if (controle > PWM_MAX_LIMIT)
-    controle = PWM_MAX_LIMIT;
-  if (controle < -PWM_MAX_LIMIT)
-    controle = -PWM_MAX_LIMIT;
+  //if (controle > 255)
+    //controle = 255;
+  //if (controle < -255)
+    //controle = -255;
     
   // if (digitalRead(BUTAO_PIN_PWM) == 0) {
-  PWM = min(abs(controle), 160);
+  PWM = min(abs(controle), 255);
   aciona_motor(controle, PWM);
   // }4
 }
@@ -154,41 +156,51 @@ void aciona_motor(float controle, int PWM)
 {
   if (controle >= 0)
   {
-    digitalWrite(PONTE_H_1, LOW);
-    analogWrite(PONTE_H_2, HIGH);
+    digitalWrite(PONTE_H_1, HIGH);
+    digitalWrite(PONTE_H_2, LOW);
   }
   else
   {
-    analogWrite(PONTE_H_1, HIGH);
-    digitalWrite(PONTE_H_2, LOW);
+    digitalWrite(PONTE_H_1, LOW);
+    digitalWrite(PONTE_H_2, HIGH);
+
   }
   analogWrite(PONTE_H_ENABLE, PWM);
 }
 
 void show_data(void)
 {
-  Serial.print((float)ultimoTempo / 1000, 3);
-  Serial.print("\t");
+  //Serial.print("tempo:");
+  //Serial.print((float)ultimoTempo / 1000, 3);
+  //Serial.print("\t");
+  Serial.print("setpoint:");
   Serial.print(sp);
   Serial.print("\t");
   // Serial.print(TempoTotalCalculo);
   // Serial.print("\t");
-  // Serial.print(PWM);
-  // Serial.print("\t");
+  Serial.print("PWM:");
+  Serial.print(PWM);
+  Serial.print("\t");
   // Serial.print(QtdPulsosCanalA);
   // Serial.print("\t");
+  Serial.print("pulsos periodo:");
   Serial.print(QtdPulsoPorPeriodo);
   Serial.print("\t");
   // Serial.print(theta);
   // Serial.print("\t");
+  Serial.print("velocidade rpm:");
   Serial.print(omega);
   Serial.print("\t");
+  Serial.print("erro:");
   Serial.print(erro);
   Serial.print("\t");
+  Serial.print("acao p:");
   Serial.print(acaoP);
   Serial.print("\t");
+  Serial.print("acao i:");
   Serial.print(acaoI, 4);
   Serial.print("\t");
+  Serial.print("controle:");
   Serial.print(controle);
 
   Serial.print("\n");
