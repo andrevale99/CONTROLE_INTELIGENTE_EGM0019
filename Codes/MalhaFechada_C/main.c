2cd865b034a16e0a867e0c21ec6f1a5438b05e60
andrevale99 <andrefvale@gmail.com>
2025-05-09 14:03:28 -0300

    Periodo de amostragem e coleta da quantidade de pulsos do encoder.

--- Codes/MalhaFechada_C/main.c
+++ Codes/MalhaFechada_C/main.c
@@ -2,6 +2,7 @@
 
 #include <stdint.h>
 #include <stdio.h>
+#include <math.h>
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -10,23 +11,60 @@
 #define BAUD 9600
 #define MYUBRR F_CPU / 16 / BAUD - 1
 
+#define BUFFER_MAX_LEN 32
+
+#define PERIODO_DE_AMOSTRAGEM 20
+
+#define ENCODER_A PD2
+
+// Pode trocar para outro pino digital,
+// caso queira desocupar o Pino INT1
+#define ENCODER_B PD3
+
+#define ROTOR_SNETIDO_HORARIO(pwm) \
+  OCR0A = (uint8_t)pwm;            \
+  OCR0B = 0;
+#define ROTOR_SNETIDO_ANTIHORARIO(pwm) \
+  OCR0A = 0;                           \
+  OCR0B = (uint8_t)pwm;
+
+#define RESOLUCAO (float)1024.
+
 //=====================================================
 //  VARIAVEIS GLOBAIS
 //=====================================================
 
 volatile uint32_t Pulsos = 0;
+volatile uint32_t teste = 0;
 
-char buffer[32];
+char buffer[BUFFER_MAX_LEN];
 
 //=====================================================
 //  FUNCS
 //=====================================================
 
+/// @brief Configuracao do pwm para o motor
 void pwm_setup(void);
+
+/// @brief Configuracao da interrupcao para
+/// ler os sensores encoder
 void external_intr_setup(void);
 
+/// @brief Configuracao do TIMER1 para
+/// fazer o periodo de amostragem
+///
+/// @param quantidade_de_dados quantos dados quer ler por segundo
+void timer_setup(uint16_t quantidade_de_dados);
+
+/// @brief Configuracao do periferico UART
+/// @param ubrr Baud Rate da comunicacao
 void USART_Init(unsigned int ubrr);
+
+/// @brief Funcao para mandar um dado pela Serial
+/// @param data o caractere que deseja mandar
 void USART_Transmit(unsigned char data);
+
+ISR(TIMER1_COMPA_vect);
 
 ISR(INT0_vect);
 
@@ -39,47 +77,14 @@ int main(void)
   pwm_setup();
   external_intr_setup();
   USART_Init(MYUBRR);
+  timer_setup(PERIODO_DE_AMOSTRAGEM);
+
+  ROTOR_SNETIDO_ANTIHORARIO(254);
 
   sei();
 
   while (1)
   {
-    PORTD |= (1 << PD6) & ~(1 << PD5);
-    for (uint8_t i = 0; i < 255; ++i)
-    {
-      OCR0A = i;
-      _delay_ms(10);
-    }
-
-    for (uint8_t i = 254; i > 0; --i)
-    {
-      OCR0A = i;
-      _delay_ms(10);
-    }
-
-    PORTD |= (1 << PD5) & ~(1 << PD6);
-    for (uint8_t i = 0; i < 255; ++i)
-    {
-      OCR0B = i;
-      _delay_ms(10);
-    }
-
-    for (uint8_t i = 254; i > 0; --i)
-    {
-      OCR0B = i;
-      _delay_ms(10);
-    }
-
-    sprintf(buffer, "HELLO WORLD %ld\n", Pulsos);
-
-    // Desativando todas as interrrupcao, nao eh legal fazer isso
-    // Deve desativar uma interrupcao expecifica (melhor maneira)
-    cli();
-    for (uint8_t i = 0; buffer[i] != '\0'; ++i)
-      USART_Transmit(buffer[i]);
-    sei();
-
-    _delay_ms(100);
   }
 }
 
@@ -89,19 +94,20 @@ int main(void)
 
 void pwm_setup()
 {
-  // Configurar PD6 (OC0A) como saída
+  // Configurar PD6 (OC0A) e
+  // PD5 (OC0B) como saída
   DDRD |= (1 << PD5) | (1 << PD6);
 
   // Modo Fast PWM com TOP=0xFF
   // COM0A1:0 = 10 → Clear OC0A on Compare Match (não-invertido)
+  // COM0B1:0 = 10 → Clear OC0B on Compare Match (não-invertido)
   // WGM01:0 = 11 → Fast PWM
-  // TCCR0A: COM0A1=1, COM0A0=0, WGM01=1, WGM00=1
   TCCR0A = (1 << COM0A1) | (1 << COM0B1) | (1 << WGM01) | (1 << WGM00);
 
-  // TCCR0B: CS01=1, CS00=1 (prescaler = 64), WGM02=0 (não usado neste modo)
+  // TCCR0B: CS01=1, CS00=1 (prescaler = 64)
   TCCR0B = (1 << CS01) | (1 << CS00);
 
-  // Duty cycle inicial de 50%
+  // Comparaca para ver os periodos
   OCR0A = 0;
   OCR0B = 0;
 }
@@ -115,10 +121,27 @@ void external_intr_setup(void)
   EIMSK |= (1 << INT0);
 }
 
+void timer_setup(uint16_t quantidade_de_dados)
+{
+  TCCR1B |= (1 << WGM12) | (1 << CS12);
+  TIMSK1 |= (1 << OCIE1A);
+  OCR1A = (uint16_t)62500 / quantidade_de_dados;
+}
+
+ISR(TIMER1_COMPA_vect)
+{
+  sprintf(buffer, "%ld\n", Pulsos);
+  for (uint8_t i = 0; buffer[i] != '\0'; ++i)
+    USART_Transmit(buffer[i]);
+  Pulsos = 0;
+}
+
 ISR(INT0_vect)
 {
-  Pulsos++;
-  PORTB ^= (1 << PB5);
+  if ((PIND & 0x0C))
+    Pulsos++;
+  else
+    Pulsos--;
 }
 
 /**
